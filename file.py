7167e6171add1570348cf7ff15e3237485543a04
metin02oktay <metinoktaybeyaz811@gmail.com>
2020-01-03 20:30:03 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-# !/usr/bin/env python3
+# !/usr/bin/env python3
+
 from flask import Flask, request
 import telepot.namedtuple
 from telepot.loop import OrderedWebhook
-import requests
-#ne eklemek isterseniz eðer kurulu deðil ise requirements.txt satýr olarak ekleyin youtube-dl,mechanize,bs4,....
-TOKEN='TELEGRAM-TOKEN'
+import requests
 
+#ne eklemek isterseniz eÄŸer kurulu deÄŸil ise requirements.txt satÄ±r olarak ekleyin youtube-dl,mechanize,bs4,....
+
+
+TOKEN='TELEGRAM-TOKEN'
+def selam():
+    bot.sendMessage(chat_id,"iyim, sen nasÄ±lsÄ±n \xF0\x9F\x98\x8C ",parse_mode='Markdown') 
+    
 
 def handle(msg):
     global chat_id
@@ -21,24 +27,31 @@ def handle(msg):
         user = (m.chat[4]+" "+m.chat[5])
     
     if (("text" in msg) !=  True):
-        bot.sendMessage(chat_id, f'*Hahaha gondegin emoji cok komik {user}*\n',parse_mode='Markdown',reply_to_message_id=msg_id)
+        bot.sendMessage(chat_id, f'Hahaha gÃ¶nderdiÄŸin emoji Ã§ok komik \xF0\x9F\x98\x82	*{user}*\n',parse_mode='Markdown',reply_to_message_id=msg_id)
         
-    else:        #gelen metin mesajý: msg['text']  onu geri gönderelim
+    elif msg['text'].upper() == "nasÄ±lsÄ±n?":
+        selam()
+    else:
+        #gelen metin mesajÄ±: msg['text']  onu geri gÃ¶nderelim, 
         bot.sendMessage(chat_id, msg['text'],parse_mode='Markdown',disable_web_page_preview=True)
 
      
 print ('Bot Aktif ...')
 PORT = int(os.environ.get('PORT', 8443))                       
-app = Flask(__name__, static_url_path = "", static_folder = "./") #static folder, resim,dosya vs flask ile açmak için                         
+app = Flask(__name__, static_url_path = "", static_folder = "./") #static folder, resim,dosya vs flask ile aÃ§mak iÃ§in                         
 bot = telepot.Bot(TOKEN)
 webhook=OrderedWebhook(bot, {'chat': handle})
 
 @app.route('/', methods=['GET'])
 def index():
+    #ana sayfaya botun telegram linkini ekleyebilirsiniz
     return "<p>Merhaba, bu bir telegram botu</p><p><a href='https://t.me/......'>Botu baslat</a></p>" 
 
 @app.route('/videolar', methods=['GET', 'POST'])
 def index3():
+    #https://APPNAME.herokuapps.com/videolar?isim=video1&tur=mp4 
+    isim=str(request.args.get("isim"))  #video1 dÃ¶nderir
+    tur=str(request.args.get("tur"))   #mp4 dÃ¶nderir
     return "video sayfasi"
    
 @app.route("/webhook", methods=['GET', 'POST'])
@@ -47,7 +60,7 @@ def pass_update():
     return 'OK'
 if __name__ == '__main__':
     try:
-        bot.setWebhook("https://<APP-NAME>.herokuapp.com/webhook") #APPNAME yazan yere heroku da kurduðunuz makinanýn ismini verin, telegramla iletiþim için
+        bot.setWebhook("https://<APP-NAME>.herokuapp.com/webhook") #APPNAME yazan yere heroku da kurduÄŸunuz makinanÄ±n ismini verin, telegramla iletiÅŸim iÃ§in
     # Sometimes it would raise this error, but webhook still set successfully.
     except telepot.exception.TooManyRequestsError:
         pass
